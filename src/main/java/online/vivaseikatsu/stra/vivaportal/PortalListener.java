e9c35de910a0d8529bbfcfeec21935a58efddacc
s-tra <nanchattekagaku@gmail.com>
2023-06-24 15:01:55 +0900

    初めてのPush

---
+++ src/main/java/online/vivaseikatsu/stra/vivaportal/PortalListener.java
@@ -0,0 +1,308 @@
+package online.vivaseikatsu.stra.vivaportal;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityPortalEvent;
+import org.bukkit.event.entity.PlayerLeashEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerUnleashEntityEvent;
+
+public class PortalListener implements Listener {
+
+    private final VivaPortal plg;
+
+    public PortalListener(VivaPortal plg_){
+        plg = plg_;
+    }
+
+    // selectコマンドによるブロック選択
+    @EventHandler
+    private void onPlayerInteractEvent(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+        // コマンド制御用のハッシュマップをみる、名前があってtrueの場合のみ続行
+        if(!plg.SelectCommand.containsKey(p.getName())) return;
+        if(!plg.SelectCommand.get(p.getName())) return;
+        // クリックしたブロックが取得できない場合は終了
+        if(e.getClickedBlock() == null) return;
+
+        // クリックしたブロックのロケーションをブロック用ハッシュマップに格納
+        plg.SelectedBlock.put(p.getName(),e.getClickedBlock().getLocation());
+        p.sendMessage(ChatColor.GRAY + "[vivaPortal] ブロックを選択しました。");
+
+        // 殴ったのをなかったことに
+        e.setCancelled(true);
+
+        // コマンド制御用マップからプレイヤーを削除
+        plg.SelectCommand.remove(p.getName());
+
+    }
+
+    // ポータルに重なったとき、ワープさせる
+    @EventHandler
+    private void onPlayerMoveEvent(PlayerMoveEvent e){
+
+        // プレイヤーを取得
+        Player p = e.getPlayer();
+
+        //移動先がnullの場合(首を回したとか)は、終了
+        if(e.getTo() == null) return;
+
+        // 同じブロック内での移動だった場合、終了
+        if(e.getFrom().getBlockX() == e.getTo().getBlockX()
+                && e.getFrom().getBlockY() == e.getTo().getBlockY()
+                && e.getFrom().getBlockZ() == e.getTo().getBlockZ()
+        ) return;
+
+        // 権限がないプレイヤーだった場合は、終了
+        if(!p.hasPermission("vivaportal.use")) return;
+
+        // ポータルの場所を取得
+        Location main2res = plg.Portal2Location("main2res");
+        Location res2main = plg.Portal2Location("res2main");
+
+        // 移動元のブロックがポータル内の場合は、終了
+        if(isPortalLocation(e.getFrom(),main2res)) return;
+        if(isPortalLocation(e.getFrom(),res2main)) return;
+
+        // ポータル"main2res"と移動先ブロックがかぶったとき (メイン -> 資源)
+        if(isPortalLocation(e.getTo(),main2res)){
+            PlayerPortalTP(p,"res2main");
+            return;
+        }
+
+        // ポータル"res2main"と移動先ブロックがかぶったとき (資源 -> メイン)
+        if(isPortalLocation(e.getTo(),res2main)){
+            PlayerPortalTP(p,"main2res");
+            return;
+        }
+
+
+        // ポータル"main2res"にMobを押し込んだときの処理
+        if(isNearThePortal(e.getTo(),main2res,5)){
+            MobPortalTP(main2res,res2main);
+            return;
+        }
+
+        // ポータル"res2main"にMobを押し込んだときの処理
+        if(isNearThePortal(e.getTo(),res2main,5)){
+            MobPortalTP(res2main,main2res);
+            return;
+        }
+
+
+    // onPlayerMoveEventここまで
+    }
+
+
+
+
+
+
+
+    // 資源ワールドの初期スポにポータルを生成
+    public void SetResSpawnPortal(){
+
+        // 資源ワールドのワールド名を取得
+        String res_name = plg.config.getString("res_world_name","null");
+
+        // 有効なワールド名じゃない場合は、終了
+        if(!plg.getServer().getWorlds().toString().contains(res_name)) return;
+
+        // 資源ワールドのスポーン座標を取得
+        Location spawn = plg.getServer().getWorld(res_name).getSpawnLocation();
+        // スポーン地点の足元を指定
+        spawn.setY(spawn.getY() -1);
+
+        // config.ymlに値をセット
+        plg.config.set("res2main.world_name", spawn.getWorld().getName());
+        plg.config.set("res2main.x", (int) spawn.getX());
+        plg.config.set("res2main.y", (int) spawn.getY());
+        plg.config.set("res2main.z", (int) spawn.getZ());
+
+        // config.ymlに書き込み
+        plg.saveConfig();
+
+        // ポータルのハリボテを生成
+        plg.CreateGateway(plg.Portal2Location("res2main"));
+
+    }
+
+    // 受け取ったロケーションがポータルの範囲内（タテ3ブロック）にあるかを返す
+    public boolean isPortalLocation(Location location , Location portal){
+
+        // ワールドが取得できない場合はfalse
+        if(location.getWorld() == null) return false;
+        if(portal.getWorld() == null) return false;
+
+        // ワールドが違えば、違う
+        if(location.getWorld() != portal.getWorld()) return false;
+        // xが違えば、違う
+        if(location.getBlockX() != portal.getBlockX()) return false;
+        // zが違えば、違う
+        if(location.getBlockZ() != portal.getBlockZ()) return false;
+
+        // ここまで来れば、x,zが一致している
+
+        // yが範囲内なら、true
+        if(location.getBlockY() == portal.getBlockY()) return true;
+        if(location.getBlockY() == portal.getBlockY() + 1) return true;
+        if(location.getBlockY() == portal.getBlockY() + 2) return true;
+
+        // 結局違えば、false
+        return false;
+
+    }
+
+    // 受け取ったロケーションがポータルの近くにあるかを返す（タテ3ブロック,周囲任意のブロック数）
+    public boolean isNearThePortal(Location location , Location portal , int radius){
+
+        // ワールドが取得できない場合はfalse
+        if(location.getWorld() == null) return false;
+        if(portal.getWorld() == null) return false;
+
+        // ワールドが違えば、違う
+        if(location.getWorld() != portal.getWorld()) return false;
+
+        // 指定された半径以内じゃない場合、false
+        if(plg.getDistance( location.getBlockX(),
+                location.getBlockZ() , portal.getBlockX(), portal.getBlockZ() ) >= radius) return false;
+
+        // ここまで来れば、x,zが範囲内にある
+
+        // 高さが範囲内なら、true
+        if(portal.getBlockY() - 3 <= location.getBlockY()
+                || location.getBlockY() <= portal.getBlockY() + 3 ) return true;
+
+
+        // 結局違えば、false
+        return false;
+
+    }
+
+    // 指定したブロックの周りを確認し、隣接して2マス分の空間がある場所を返す。なかった場合はもとのブロックを返す。
+    public Location TPpointCheck(Location center){
+
+        Location exitBlock = center.clone();
+
+        for(int x = -1; x <= 1; x++){
+            for(int z = 1; z >= -1; z--){
+
+                // xないしzが0のときはcontinue
+                if(x == 0 || z == 0) continue;
+
+                // チェックするブロックを設定
+                Location check = center.clone();
+                check.setX(check.getX() + (double) x);
+                check.setZ(check.getZ() + (double) z);
+
+                Location check2 = check.clone();
+                check2.setY(check.getY() + 1);
+
+                // チェック先がタテに2マスAIRなら、隣接する場所の確認
+                if(check.getBlock().getType() == Material.AIR
+                        && check2.getBlock().getType() == Material.AIR){
+
+                    // チェックするブロックを設定
+                    check = center.clone();
+                    check.setZ(check.getZ() + (double) z);
+
+                    check2 = check.clone();
+                    check2.setY(check.getY() + 1);
+
+                    if(check.getBlock().getType() == Material.AIR
+                            && check2.getBlock().getType() == Material.AIR){
+                        exitBlock = check.clone();
+                        break;
+                    }
+
+                    // チェックするブロックを設定
+                    check = center.clone();
+                    check.setX(check.getX() + (double) x);
+
+                    check2 = check.clone();
+                    check2.setY(check.getY() + 1);
+
+                    if(check.getBlock().getType() == Material.AIR
+                            && check2.getBlock().getType() == Material.AIR){
+                        exitBlock = check.clone();
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        return exitBlock;
+
+    }
+
+
+    // プレイヤーテレポートの処理
+    public void PlayerPortalTP(Player player,String exitPortal){
+
+        // 資源側のポータルがない場合、ポータルをスポーン地点に自動生成
+        if(plg.config.getString("res2main.world_name") == null){
+            SetResSpawnPortal();
+        }
+
+        // テレポート先が不正な場合
+        if(plg.Portal2Location(exitPortal).getWorld() == null){
+            player.sendMessage(ChatColor.GRAY + "[vivaPortal] "+ exitPortal +"の設定がされていません！");
+            return;
+        }
+
+        // プレイヤーをテレポート
+        player.teleport(plg.Portal2Location(exitPortal));
+
+        // ポータルのハリボテを生成
+        plg.CreateGateway(plg.Portal2Location(exitPortal));
+
+    }
+
+    // Mobのテレポートの処理
+    public void MobPortalTP(Location enterPortal,Location exitPortal){
+
+        // 相手側ポータルが不正な場合、終了
+        if(exitPortal.getWorld() == null) return;
+
+        // TP先の設定
+        Location exitBlock = exitPortal.clone();
+
+        // ポータルに重なってるMobを探して、テレポートさせる
+        for(Entity mob : enterPortal.getChunk().getEntities() ){
+
+            if(isPortalLocation(mob.getLocation(),enterPortal)){
+                // ポータル周りのAirブロックの場所をTP先に選定
+                exitBlock = TPpointCheck(exitBlock);
+                if(isPortalLocation(exitBlock,exitPortal)){
+                    exitBlock.setY(exitBlock.getY() +1);
+                    exitBlock = TPpointCheck(exitBlock);
+                }
+
+                // TP先にmobをテレポート
+                mob.teleport(exitBlock);
+
+            }
+
+        }
+
+
+    }
+
+
+
+
+}
---
+++ src/main/java/online/vivaseikatsu/stra/vivaportal/VivaPortal.java
@@ -0,0 +1,335 @@
+package online.vivaseikatsu.stra.vivaportal;
+
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.*;
+
+public final class VivaPortal extends JavaPlugin {
+
+    public FileConfiguration config;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        // listenerの処理を行うクラスを宣言
+        getServer().getPluginManager().registerEvents(new PortalListener(this),this);
+
+        // config.ymlの準備
+        // config.ymlがない場合はファイルを出力
+        saveDefaultConfig();
+
+        // config.ymlの読み込み
+        config = getConfig();
+
+        getLogger().info("プラグインが有効になりました。");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("プラグインが無効になりました。");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        // ポータルの作成、削除のコマンド
+        if(command.getName().equalsIgnoreCase("vivaportal")){
+
+            if(sender instanceof Player){
+                if(!sender.hasPermission("vivaportal.setting")){
+                    sender.sendMessage(ChatColor.GRAY +"[vivaPortal] 権限がありません!");
+                }
+            }
+
+            // オプションがない場合
+            if(args.length == 0){
+                sender.sendMessage(ChatColor.GRAY +"[vivaPortal] オプションを指定してください!");
+                sender.sendMessage(ChatColor.GRAY +"[vivaPortal] /vivaportal help");
+                return true;
+            }
+
+            // selectオプション
+            if(args[0].equalsIgnoreCase("select")) {
+                // senderがプレイヤーじゃないとき
+                if(!(sender instanceof Player)){
+                    sender.sendMessage(ChatColor.GRAY +"[vivaPortal] プレイヤーのみが実行可能な処理です。");
+                    return true;
+                }
+                Player p = (Player) sender;
+                // セレクトコマンドを実行したフラグをたてる
+                SelectCommand.put(p.getName(),true);
+                // ブロックを選択するようにメッセージ
+                p.sendMessage(ChatColor.GRAY + "[vivaPortal] 選択したいブロックをクリックしてください。");
+                return true;
+            }
+
+            // regenオプション
+            if(args[0].equalsIgnoreCase("regen")) {
+                // main2resの再生成
+                if(config.getString("main2res") != null){
+                    RemoveGateway(Portal2Location("main2res"));
+                    CreateGateway(Portal2Location("main2res"));
+                }
+                // res2mainの再生成
+                if(config.getString("res2main") != null){
+                    RemoveGateway(Portal2Location("res2main"));
+                    CreateGateway(Portal2Location("res2main"));
+                }
+
+                // 再生成完了のメッセージ
+                sender.sendMessage(ChatColor.GRAY + "[vivaPortal] ポータルの構造物を再生成しました。");
+                return true;
+            }
+
+            // removeオプション
+            if(args[0].equalsIgnoreCase("remove")){
+
+                // 引数がないとき
+                if(args.length == 1) {
+                    sender.sendMessage(ChatColor.GRAY +"[vivaPortal] 有効なポータルを指定してください!");
+                    return true;
+                }
+
+                // 有効なポータルが指定されなかったとき
+                if(!args[1].equals("main2res") &
+                        !args[1].equals("res2main")){
+                    sender.sendMessage(ChatColor.GRAY +"[vivaPortal] 有効なポータルを指定してください!");
+                    return true;
+                }
+
+                // 指定されたポータル名を格納
+                String portal_name = args[1];
+
+                // 古いポータルが存在する場合、設定を削除
+                if(config.getString(portal_name + ".world_name") != null){
+
+                    // ポータルの構造物を削除(読み込み済みの場合のみ)
+                    RemoveGateway(Portal2Location(portal_name));
+
+                    // configの内容を書き換え
+                    config.set(portal_name + ".world_name", null);
+                    config.set(portal_name + ".x", null);
+                    config.set(portal_name + ".y", null);
+                    config.set(portal_name + ".z", null);
+                    config.set(portal_name + ".direction", null);
+                    // config.ymlに書き込み
+                    saveConfig();
+
+                    // 削除の通知
+                    sender.sendMessage(ChatColor.GRAY +"[vivaPortal] " + portal_name + "を削除しました。");
+                    return true;
+                }
+
+                sender.sendMessage(ChatColor.GRAY +"[vivaPortal] " + portal_name + "はすでに削除されています。");
+                return true;
+
+            // removeここまで
+            }
+
+            // setオプション
+            if(args[0].equalsIgnoreCase("set")) {
+
+                // senderがプレイヤーじゃないとき
+                if(!(sender instanceof Player)){
+                    sender.sendMessage(ChatColor.GRAY +"[vivaPortal] プレイヤーのみが実行可能な処理です。");
+                    return true;
+                }
+
+                Player p = (Player) sender;
+
+                // 引数がないとき
+                if(args.length < 2) {
+                    p.sendMessage(ChatColor.GRAY +"[vivaPortal] 有効なポータルを指定してください!");
+                    return true;
+                }
+
+                // 有効なポータルが指定されなかったとき
+                if(!args[1].equals("main2res") &
+                        !args[1].equals("res2main")){
+                    p.sendMessage(ChatColor.GRAY +"[vivaPortal] 有効なポータルを指定してください!");
+                    return true;
+                }
+
+                // ブロック選択がされていないとき
+                if(!SelectedBlock.containsKey(p.getName())){
+                    p.sendMessage(ChatColor.GRAY +"[vivaPortal] ポータルを設置するブロックを指定してください!");
+                    return true;
+                }
+                if(SelectedBlock.get(p.getName()) == null){
+                    p.sendMessage(ChatColor.GRAY +"[vivaPortal] ポータルを設置するブロックを指定してください!");
+                    return true;
+                }
+
+
+                // 指定されたポータル名を格納
+                String portal_name = args[1];
+
+                // 古いポータルが存在する場合、その構造を削除(読み込まれているときのみ)
+                RemoveGateway(Portal2Location(portal_name));
+
+                // 選択済みのブロックのロケーションを取得
+                Location l_new = SelectedBlock.get(p.getName());
+
+                // config.ymlに値をセット
+                config.set(portal_name + ".world_name", l_new.getWorld().getName());
+                config.set(portal_name + ".x", (int) l_new.getX());
+                config.set(portal_name + ".y", (int) l_new.getY());
+                config.set(portal_name + ".z", (int) l_new.getZ());
+
+                // args[2] が参照できるときのみ参照する
+                if(args.length >= 3) {
+                    if(args[2].equalsIgnoreCase("n")){
+                        config.set(portal_name + ".direction", 180);
+                    }
+                    if(args[2].equalsIgnoreCase("s")){
+                        config.set(portal_name + ".direction", 0);
+                    }
+                    if(args[2].equalsIgnoreCase("e")){
+                        config.set(portal_name + ".direction", 270);
+                    }
+                    if(args[2].equalsIgnoreCase("w")){
+                        config.set(portal_name + ".direction", 90);
+                    }
+                }
+
+                // config.ymlに書き込み
+                saveConfig();
+
+                // ポータルのハリボテを生成
+                CreateGateway(Portal2Location(portal_name));
+
+                // 選択したブロックを忘れる
+                SelectedBlock.remove(p.getName());
+
+                p.sendMessage(ChatColor.GRAY + "[vivaPortal] " + portal_name + "の設定が完了しました。");
+
+                return true;
+
+            // setオプションここまで
+            }
+
+
+            // 有効なオプションがしていされなかったとき or Help
+            if(args[0].equalsIgnoreCase("help")){
+                sender.sendMessage(ChatColor.GRAY +"[vivaPortal] Help");
+            }else{
+                sender.sendMessage(ChatColor.GRAY +"[vivaPortal] 有効なオプションを指定してください!");
+            }
+            sender.sendMessage(ChatColor.GRAY +"/vivaportal select");
+            sender.sendMessage(ChatColor.GRAY +"/vivaportal set <main2res/res2main> <n,s,e,w>");
+            sender.sendMessage(ChatColor.GRAY +"/vivaportal regen");
+            sender.sendMessage(ChatColor.GRAY +"/vivaportal remove <main2res/res2main>");
+            return true;
+
+        // vivaPortalここまで
+        }
+
+
+        return true;
+    }
+
+// こまごましたやつ
+
+    // コマンド制御用ハッシュマップを作成
+    public HashMap<String, Boolean> SelectCommand = new HashMap<String, Boolean>();
+    // 選択されたブロック用のハッシュマップ
+    public HashMap<String,Location> SelectedBlock = new HashMap<String, Location>();
+
+    // チャンクの距離を計算
+    public int getDistance(int x1, int y1, int x2, int y2) {
+        return (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+
+    // ポータル名からロケーションを取得
+    public Location Portal2Location(String portal_name){
+
+        String world_name = config.getString(portal_name + ".world_name","null");
+        double x = config.getDouble(portal_name + ".x",0) + 0.5;
+        double y = config.getDouble(portal_name + ".y",65) + 1;
+        double z = config.getDouble(portal_name + ".z",0) + 0.5;
+        float  d = config.getInt(portal_name + ".direction",0);
+
+        return new Location(getServer().getWorld(world_name),x,y,z,d,0);
+    }
+
+
+    // ゲートウェイの構造を生成
+    public void CreateGateway(Location location){
+
+        // ポータルの場所が読み込まれてない場合、終了
+        if(!location.isWorldLoaded()) return;
+
+        // ポータルがあるワールドを取得
+        World w = location.getWorld();
+
+        // 足元の岩盤の座標を取得
+        int x = location.getBlockX();
+        int y = location.getBlockY() - 1;
+        int z = location.getBlockZ();
+
+        // 足元の岩盤にするブロックから上に向かって生成
+        w.getBlockState(x,y,z).getBlock().setType(Material.BEDROCK);
+        w.getBlockState(x,y+1,z).getBlock().setType(Material.END_GATEWAY);
+        w.getBlockState(x,y+2,z).getBlock().setType(Material.END_GATEWAY);
+        w.getBlockState(x,y+3,z).getBlock().setType(Material.END_GATEWAY);
+        w.getBlockState(x,y+4,z).getBlock().setType(Material.BEDROCK);
+
+    }
+
+    // ゲートウェイ構造の削除
+    public void RemoveGateway(Location location){
+
+        // ポータルの場所が読み込まれてない場合、終了
+        if(!location.isWorldLoaded()) return;
+
+        // ポータルがあるワールドを取得
+        World w = location.getWorld();
+
+        // 足元の岩盤の座標を取得
+        int x = location.getBlockX();
+        int y = location.getBlockY() - 1;
+        int z = location.getBlockZ();
+
+        // 構造が存在していない場合は、終了
+        if(!(w.getBlockState(x,y,z).getBlock().getType() == Material.BEDROCK)) return;
+        if(!(w.getBlockState(x,y+4,z).getBlock().getType() == Material.BEDROCK)) return;
+
+
+        // 足元の岩盤にするブロックから上に向かって生成
+        w.getBlockState(x,y,z).getBlock().setType(Material.STONE);
+        w.getBlockState(x,y+1,z).getBlock().setType(Material.AIR);
+        w.getBlockState(x,y+2,z).getBlock().setType(Material.AIR);
+        w.getBlockState(x,y+3,z).getBlock().setType(Material.AIR);
+        w.getBlockState(x,y+4,z).getBlock().setType(Material.AIR);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
